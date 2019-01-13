938d637d67d731e9a5bb028f6047ad893c6cf95d
David.Rodrigues <david@sixhat.net>
2019-01-13 23:13:32 +0000

    better alias

--- src/color.js
+++ src/color.js
@@ -3,15 +3,10 @@ class Dave {
   constructor() {
     this.grid = new Grid();
     this.g = this.grid;
+    this.f = this.fillHsluv;
+    this.s = this.strokeHsluv;
   }
-  // alias for fillHsluv
-  f(h, s, l) {
-    this.fillHsluv(h, s, l);
-  }
-  // alias for strokeHsluv
-  s(h, s, l) {
-    this.strokeHsluv(h, s, l);
-  }
+  
   // sets fill according to hsluv parameters
   fillHsluv(h, s, l) {
     let rgb = hsluv.hsluvToRgb([h, s, l]);
