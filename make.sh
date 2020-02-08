e2f2d1fa724b098b67a36fe94a67d5b90e508462
David Sousa-Rodrigues <david@sixhat.net>
2020-02-08 18:54:19 +0000

    make $(( $version + 1 ))

--- make.sh
+++ make.sh
@@ -14,3 +14,7 @@ coffee -cb src/*.coffee
 echo // utils.dave.p5.js v.$(($version + 1 )) > $out
 cat src/*.js >> $out
 echo $(( $version + 1 )) > version.txt
+
+git commit -a -m 'make $(( $version + 1 ))'
+git tag -a v$(( $version + 1 )) -m '$(( $version + 1 ))'
+git push
